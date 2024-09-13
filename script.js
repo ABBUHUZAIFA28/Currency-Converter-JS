7ac38eef6cf5584b5da52351a66e94d1ef395e97
ABBUHUZAIFA28 <huzaifamohammed2428@gmail.com>
2024-09-13 19:20:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+
+const BaseURL = "https://v6.exchangerate-api.com/v6/bafffeba8f7ebea7c6b90a7c/latest";
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+
+for (let select of dropdowns) {
+    for ( currCode in countryList) {
+        let newopt = document.createElement("option");
+        newopt.innerText = currCode;
+        newopt.value = currCode;
+
+        if (select.name === "from" && currCode === "USD") {
+            newopt.selected = "selected";
+        } else if (select.name === "to" && currCode === "INR") {
+            newopt.selected = "selected";
+        }
+        select.append(newopt);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateExchangeRate = async () => {
+    let amt = document.querySelector(".amount input");
+    let amtVal = amt.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amt.value = "1";
+    }
+    const URL = `${BaseURL}/${fromCurr.value}`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data.conversion_rates[toCurr.value];
+
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
